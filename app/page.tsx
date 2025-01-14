e835db7b8005a74d78dd8771e8d1510f9e911a9d
Yashar Mhd <yasharm@Yashars-MacBook-Air.local>
2025-01-14 15:30:39 +0100

    Fix file structure and component paths

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
+'use client'
 import dynamic from 'next/dynamic'
 
 const EmbodiedEnergyCalculator = dynamic(
-  () => import('@/components/EmbodiedEnergyCalculator'),
+  () => import('../components/EmbodiedEnergyCalculator'),
   { ssr: false }
 )
 
 export default function Home() {
   return (
-    <main className="p-8">
+    <main className="flex min-h-screen flex-col items-center justify-between p-8">
+      <EmbodiedEnergyCalculator />
+    </main>
+  );
+'use client'
+import dynamic from 'next/dynamic'
+
+const EmbodiedEnergyCalculator = dynamic(
+  () => import('../components/EmbodiedEnergyCalculator'),
+  { ssr: false }
+)
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-8">
       <EmbodiedEnergyCalculator />
     </main>
   );
